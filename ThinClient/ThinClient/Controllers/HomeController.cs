23bec4da2cd7070b5a0f19e33c45a41f341092d7
ttorok <tamas.torok@comicsys.hu>
2016-05-24 11:01:07 +0200

    That works fine

---
+++ ThinClient/ThinClient/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace ThinClient.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ ThinClient/ThinClient/Models/ConfigModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThinClient.Models
+{
+    public class ConfigModel
+    {
+        public string ApplicationName {get; set;}
+        public string ApplicationNameFull { get; set; }
+        public string Version { get; set; }
+    }
+}
